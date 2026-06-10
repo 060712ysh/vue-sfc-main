aede870146fcf44fb7fb5bdc31db803963a439c5
060712yang <cxks20240401@gmail.com>
2026-06-10 16:54:34 +0800

    refactor: simplify Vite configuration and update base path

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/vue-sfc/' : '/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-}))
+// vite.config.js
+export default defineConfig({
+  plugins: [vue()],
+  base: '/vue-sfc-main/', // 👈 記得要改成 vue-sfc-main 喔！
+})
